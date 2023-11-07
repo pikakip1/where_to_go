5f085a05678cae75f0bd749e6a472c1c1a83f5c0
pikak <pikakip222@mail.ru>
2023-11-07 15:47:22 +0300

    Add frontend

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]

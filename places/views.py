9184517366bec705ce205ff85f019d715afb782b
pikak <pikakip222@mail.ru>
2023-11-08 18:03:04 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def places_info():
+    return {"type": "FeatureCollection",
+            "features": [
+              {
+                "type": "Feature",
+                "geometry": {
+                  "type": "Point",
+                  "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                  "title": "«Легенды Москвы",
+                  "placeId": "moscow_legends",
+                  "detailsUrl": "static/places/moscow_legends.json"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                  "type": "Point",
+                  "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                  "title": "Крыши24.рф",
+                  "placeId": "roofs24",
+                  "detailsUrl": "static/places/roofs24.json"
+                }
+              }
+            ]
+          }
+
+
+def index(request):
+    context = {
+        'places': places_info()
+    }
+
+    return render(request, 'index.html', context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
+from places.views import index
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-def index(request):
-    return render(request, 'index.html')
 
 
 urlpatterns = [

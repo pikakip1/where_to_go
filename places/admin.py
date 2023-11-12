ade39f330cace239d8312dc36849b813cc3764e7
pikak <pikakip222@mail.ru>
2023-11-12 14:36:53 +0300

    added the ability to edit images in the place setting

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from .models import Place, PlaceImg
 
 
-@admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
-    list_display = ['title']
+class PlaceImgInline(admin.TabularInline):
+    model = PlaceImg
+    extra = 1
 
 
 @admin.register(PlaceImg)
 class PlaceImgAdmin(admin.ModelAdmin):
-    list_display = ['location_name', 'id']
+    list_display = ['location_name', 'image']
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImgInline]
+
+
+
+
+
